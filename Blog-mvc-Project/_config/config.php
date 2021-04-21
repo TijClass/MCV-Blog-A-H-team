c3d6a57d5bd572706ef90e66cf23b97361aa4151
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2021-04-21 18:44:55 +0000

    reset

--- Blog-mvc-Project/_config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define("ABS_PATH","http://localhost/Template_MVC_Version1-master");
-define("HOST","localhost");
-define("DATABASE","testdb");
-define("USER","root");
-define("PASSWORD","");
